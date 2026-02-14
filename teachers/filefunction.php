02ffb47e81c28007a8bda05061631c601534ef59
parmin-arababadi <parminaman.lov@gmail.com>
2026-02-14 11:28:57 -0800

    fopen error

--- teachers/filefunction.php
+++ teachers/filefunction.php
@@ -1,19 +1,37 @@
 <?php
 
-function write_file(
-    string $path,
-    string $value
-) {
+// function write_file(
+//     string $path,
+//     string $value
+// ) {
+//     if (file_exists($path)) {
+//         $file=fopen($path,'a+');
+//         fwrite($file,$value);
+//         fclose($file);
+//     }
+// }
+function open_file(string $path)
+{
     if (file_exists($path)) {
-        $file=fopen($path,'a+');
-        fwrite($file,$value);
-        fclose($file);
-    }
-}
-function getfile(string $path){
-    if (file_exists($path)) {
-        $file=file_get_contents($path);
+        $file = fopen($path, 'a+');
         return $file;
     }
 }
+$f=open_file('teacher_classlist.xlsx');
+echo $f;
+// function write_file(string $path, string $value)
+// {
+//     fwrite($path, $value);
+// }
+// function close_file( string $path){
+//     fclose($path);
+// }
+
+// function getfile(string $path)
+// {
+//     if (file_exists($path)) {
+//         $file = file_get_contents($path);
+//         return $file;
+//     }
+// }
 ?>
--- teachers/tclasslist.php
+++ teachers/tclasslist.php
@@ -11,20 +11,22 @@ validation($teacherid, $user_type);
 $classes = $pdo->prepare("select classes.id,class_start,class_end,week_day.title,start_date,finish_date,lesson from classes join lessons on lessons.id=classes.lesson_id join week_day on week_day.id=class_day where classes.teacher_id=:teacherid");
 $classes->execute([":teacherid" => "$teacherid"]);
 $classes = $classes->fetchAll(pdo::FETCH_ASSOC);
-foreach($classes as $class){
-    if(!empty($class['id'])){
-    $class_id=$class['id'];
-    $lesson=$class['lesson'];
-    $start_date=$class['start_date'];
-    $finish_date=$class['finish_date'];
-    $class_day=$class['title'];
-    $class_start=$class['class_start'];
-    $class_end=$class['class_end'];
-        $path="teacher_classlist.xlsx";
-        $value="$class_id , $lesson , $start_date , $finish_date , $class_day , $class_start , $class_end.PHP_EOL";
-        write_file($path,$value);
-        $file=getfile($path);
-        echo $file;
+$path = "teacher_classlist.xlsx";
+$f = open_file($path);
+foreach ($classes as $class) {
+    if (!empty($class['id'])) {
+        $class_id = $class['id'];
+        $lesson = $class['lesson'];
+        $start_date = $class['start_date'];
+        $finish_date = $class['finish_date'];
+        $class_day = $class['title'];
+        $class_start = $class['class_start'];
+        $class_end = $class['class_end'];
+        $value = "$class_id , $lesson , $start_date , $finish_date , $class_day , $class_start , $class_end.PHP_EOL";
+        write_file($f, $value);
     }
 }
+close_file($f);
+$file = getfile($path);
+echo $file;
 ?>
